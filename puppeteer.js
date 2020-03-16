eb9f8d4ced386dcbe2eafcc29f7fe73a68484542
Jaime Burbano <burbanojaime98@gmail.com>
2020-03-16 13:05:45 -0500

    Reportes de multiples HTML

--- puppeteer.js
+++ puppeteer.js
@@ -1,29 +1,47 @@
+//Libreria para generar el pdf
 const pupppeteer = require ('puppeteer');
 
-const port = 'http://127.0.0.1:5500/pagUno/'
+//Libreria para hacer merge de varias paginas en un solo pdf
+const merge = require('easy-pdf-merge');
 
+var ports = [];
+
+//Ruta de cada página
+for (let i = 14; i <= 16; i++) {
+    ports.push('http://localhost:5500/pag' + i + '/')
+}
 
 async function printPDF(){
     const browser = await pupppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({
-        width: 1920,
-        height: 1080,
-        deviceScaleFactor: 1
-    });
-    await page.goto(port);
 
-    await page.pdf({
-        path: 'report.pdf',
-        width:1920,
-        height:1080,
-        displayHeaderFooter: false,
-        printBackground: true
-    })
+    var pagesPDF = [];
+
+    //Se recorre cada página y se guardar en pagesPDF
+    for(var i = 0; i < ports.length; i++){
+        await page.goto(ports[i], {waitUntil: 'networkidle2'});
+        var pdfFileName =  'Pagina'+(i+1)+'.pdf';
+        pagesPDF.push(pdfFileName);
+        await page.pdf({path: pdfFileName, width:1920, height:1080, printBackground: true});
+      }
 
     await browser.close();
-
-  
+    await mergeMultiplePDF(pagesPDF);
 }
 
 printPDF();
+
+const mergeMultiplePDF = (pagesPDF) => {
+    return new Promise((resolve, reject) => {
+        merge(pagesPDF,'Reporte.pdf',function(err){
+
+            if(err){
+                console.log(err);
+                reject(err)
+            }
+
+            console.log('Success');
+            resolve()
+        });
+    });
+};
